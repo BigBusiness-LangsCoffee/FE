a865e2ade6bbd8014db70134e8ab2832842c93d1
gimcheolho <sm708311@gmail.com>
2023-04-03 16:20:42 +0900

    Add: 프로젝트 초기세팅

---
+++ src/Router.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "./pages/Main/Main";
+import Feed from "./pages/Feed/Feed";
+import Map from "./pages/Map/Map";
+import MyPage from "./pages/MyPage/MyPage";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/feed" element={<Feed />} />
+        <Route path="/map" element={<Map />} />
+        <Route path="/mypage" element={<MyPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;

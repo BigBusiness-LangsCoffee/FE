0a385306c6f7fa593046c6cd17d568e8a466e95b
gimcheolho <sm708311@gmail.com>
2023-04-10 14:59:13 +0900

    Modify: 전체 레이아웃

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import styled from 'styled-components';
 import Main from './pages/Main/Main';
 import Feed from './pages/Feed/Feed';
 import Map from './pages/Map/Map';
@@ -7,14 +8,22 @@ import MyPage from './pages/MyPage/MyPage';
 const Router = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/feed" element={<Feed />} />
-        <Route path="/map" element={<Map />} />
-        <Route path="/mypage" element={<MyPage />} />
-      </Routes>
+      <Content>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/feed" element={<Feed />} />
+          <Route path="/map" element={<Map />} />
+          <Route path="/mypage" element={<MyPage />} />
+        </Routes>
+      </Content>
     </BrowserRouter>
   );
 };
 
 export default Router;
+
+const Content = styled.div`
+  border: 1px solid red;
+  width: ${(props) => props.theme.laptopWidth};
+  height: ${(props) => props.theme.laptopHeight};
+`;
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,16 +1,4 @@
-import styled from 'styled-components';
-
 function Main() {
-  return (
-    <Content>
-      <div>Main</div>
-    </Content>
-  );
+  return <div>Main</div>;
 }
 export default Main;
-
-const Content = styled.div`
-  border: 1px solid red;
-  width: ${(props) => props.theme.laptopWidth};
-  height: ${(props) => props.theme.laptopHeight};
-`;

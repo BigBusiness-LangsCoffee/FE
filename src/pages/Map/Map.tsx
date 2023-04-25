4750a8cee4471e98043fb2abdb185bb0810ad675
김기윤 <ki@Kis-MacBook-Pro.local>
2023-04-26 01:25:21 +0900

    Working in progress

--- src/pages/Map/Map.tsx
+++ src/pages/Map/Map.tsx
@@ -1,7 +1,41 @@
 import './Map.scss';
 
-const Map = () => {
-  return <div>Map</div>;
+const Map: React.FC = () => {
+  return (
+    <div className="MapMain">
+      <div className="TopNav">
+        {MAP_CATEGORY.map((item) => {
+          return (
+            <button key={item.id} className="TopNavCategory">
+              {item.title}
+            </button>
+          );
+        })}
+      </div>
+      <div className="MapDisplay"></div>
+      <div className="MainNav">
+        <button className="MainNavBtn">
+          <div className="MainNavIcon">피드</div>
+          <span className="MainNavText">피드</span>
+        </button>
+        <button className="MainNavBtn">
+          <div className="MainNavIcon">주문</div>
+          <span className="MainNavText">주문</span>
+        </button>
+        <button className="MainNavBtn">
+          <div className="MainNavIcon">내 정보</div>
+          <span className="MainNavText">내 정보</span>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Map;
+
+const MAP_CATEGORY: { id: number; title: string }[] = [
+  { id: 1, title: '카테고리' },
+  { id: 2, title: '별점' },
+  { id: 3, title: '5분거리 이내' },
+  { id: 4, title: '쿠폰 있음' },
+];

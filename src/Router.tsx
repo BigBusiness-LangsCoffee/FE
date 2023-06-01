e36dbc3cb10a9496f8885a938b5e359c83d57601
김동현 <koz8615@kakao.com>
2023-06-01 23:49:48 +0900

    cropper사잔 props 값 못받아 오는 부분 해결

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,7 +6,6 @@ import MyPage from './pages/MyPage/MyPage';
 import Nav from './components/nav/Nav';
 import MypageEditMyInfo from './pages/MyPage/MypageEditMyInfo';
 import MypagePayAndAlarm from './pages/MyPage/MypagePayAndAlarm';
-import MyeditPicture from './pages/MyPage/MyeditPicture';
 
 const Router = () => {
   return (
@@ -16,11 +15,12 @@ const Router = () => {
         <Route path="/feed" element={<Feed />} />
         <Route path="/map" element={<Map />} />
         <Route path="/mypage" element={<MyPage />} />
-        <Route path="/editMyInfo" element={<MypageEditMyInfo />} />
         <Route path="/payAndAlarm" element={<MypagePayAndAlarm />} />
-        <Route path="/picturePage" element={<MyeditPicture />} />
       </Routes>
       <Nav />
+      <Routes>
+        <Route path="/editMyInfo" element={<MypageEditMyInfo />} />
+      </Routes>
     </BrowserRouter>
   );
 };
